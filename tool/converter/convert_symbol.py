5fac87e8a581d1a3cadb1794c3dcfad9faee2801
XiangruiCAI <caixr91@gmail.com>
2016-09-26 10:19:24 +0800

    create singa python script from caffe model proto

--- tool/converter/convert_symbol.py
+++ tool/converter/convert_symbol.py
@@ -13,13 +13,22 @@ except ImportError:
 
 # Set configuration of solver
 # Read caffe proto file
+def readProtoNetFile(filepath):
+    net_config = ''
+    if caffe_flag:
+        net_config = caffe.proto.caffe_pb2.NetParameter()
+    else:
+        net_config = caffe_parse.caffe_pb2.NetParameter()
+    return readProtoFile(filepath, net_config)
+
 def readProtoSolverFile(filepath):
     solver_config = ''
     if caffe_flag:
-        solver_config = caffe.proto.caffe_pb2.NetParameter()
+        solver_config = caffe.proto.caffe_pb2.SolverParameter()
     else:
-        solver_config = caffe_parse.caffe_pb2.NetParameter()
+        solver_config = caffe_parse.caffe_pb2.SolverParameter()
     return readProtoFile(filepath, solver_config)
+
 
 # Read and parse caffe proto file
 def readProtoFile(filepath, parser_object):
@@ -56,160 +65,203 @@ def convParamToString(param):
     else:
         dilate = 1 if len(param.dilation) == 0 else param.dilation[0]
     # convert to string except for dilation
-    param_string = "num_filter=%d, pad=(%d,%d), kernel=(%d,%d), stride=(%d,%d), no_bias=%s" %\
+    param_string = "num_filter=%d, pad=(%d,%d), kernel=(%d,%d), stride=(%d,%d), use_bias=%s" %\
         (param.num_output, pad, pad, kernel_size,\
-        kernel_size, stride, stride, not param.bias_term)
+        kernel_size, stride, stride, param.bias_term)
     # deal with dilation. Won't be in deconvolution
     if dilate > 1:
         param_string += ", dilate=(%d, %d)" % (dilate, dilate)
     return param_string
 
+# For Conv and Dense layers, convert weight_filler and bias_filler into
+# w_specs and b_specs of singa
+def wbParamToString(layer):
+    print layer
+    w_specs = ''
+    b_specs = ''
+    if layer.type == 'Convolution':
+        param = layer.convolution_param
+    elif layer.type == 'InnerProduct':
+        param = layer.inner_product_param
+    if len(layer.param) > 0:
+        w_specs = '''w_specs = {'init': '%s', 'mean': %d, 'std': %f, 'lr_mult': %f}''' %\
+            (param.weight_filler.type, param.weight_filler.mean, \
+            param.weight_filler.std, layer.param[0].lr_mult)
+        if len(layer.param) > 1:
+            b_specs = '''b_specs = {'init': '%s', 'mean': %d, 'std': %f, 'lr_mult': %f}''' %\
+                (param.weight_filler.type, param.weight_filler.mean, \
+                param.weight_filler.std, layer.param[1].lr_mult)
+        else:
+            b_specs = '''b_specs = {'init': '%s', 'mean': %d, 'std': %f}''' %\
+                (param.weight_filler.type, param.weight_filler.mean, \
+                param.weight_filler.std)
+    else:
+        w_specs = '''w_specs = {'init': '%s', 'mean': %d, 'std': %f}''' %\
+            (param.weight_filler.type, param.weight_filler.mean, \
+            param.weight_filler.std)
+
+    return w_specs, b_specs
+
 # Convert caffe model into a singa model
-def proto2script(proto_file):
-    proto = readProtoSolverFile(proto_file)
-    connection = dict()
-    symbols = dict()
-    top = dict()
+def net2script(net_file):
+    net = readProtoNetFile(net_file)
     flatten_count = 0
-    symbol_string = ""
     layer = ''
-    if len(proto.layer):
-        layer = proto.layer
-    elif len(proto.layers):
-        layer = proto.layers
+    # class of loss: 
+    #   0 - no loss function
+    #   1 - SoftmaxCrossEntropy
+    #   2 - SquaredError
+    loss = 0
+    # class of metric:
+    #   0 - no metric
+    #   1 - Accuracy 
+    metric = 0 
+    if len(net.layer):
+        layer = net.layer
+    elif len(net.layers):
+        layer = net.layers
     else:
         raise Exception('Invalid proto file.')
     # Get input size to network
-    input_dim = [1, 3, 224, 224] # default
-    input_dim = layer[0].input_param.shape.dim[0]
-    print layer[0].input_param.shape.dim
-    print input_dim
-    # We assume the first bottom blob of first layer is the output from data layer
-    print "layer[0]:\n", layer[0]
-    if hasattr(layer[0], bottom):
-        print layer[0].bottom[0]
-    else:
-        print "hello"
+    #input_dim = [1, 3, 224, 224] # default
+    #input_dim = layer[0].input_param.shape[0].dim
+    #print "Shape of input tensor: ", input_dim
 
-    # layer[0] may have no bottom, why did they call this attribute?
-    input_name = layer[0].bottom[0]
-    output_name = ""
-    mapping = {input_name : 'data'}
-    need_flatten = {input_name : False}
-    # I should know the structure of SIGNA layer classes.
+    #TODO: generate input tensors based on prototxt file
+
+    #if hasattr(layer[0], top):
+    #    input_name = layer[0].top[0]
+    #else:
+    #    input_name = layer[0].name
+
+    # record layer mapping from caffe to singa
+    #mapping = {input_name : 'data'}
+    #need_flatten = {input_name : False}
+    output = ''
+
     for i in range(len(layer)):
         type_string = ''
         param_string = ''
+        # replace '-' or '/' by '_'
         name = re.sub('[-/]', '_', layer[i].name)
+        if layer[i].type == 'Data' or layer[i].type == 5:
+            # Ignore data layer(s)
+            continue
         if layer[i].type == 'Convolution' or layer[i].type == 4:
-            type_string = 'singa.layer.Conv2D'
+            type_string = 'layer.Conv2D'
             param_string = convParamToString(layer[i].convolution_param)
-            need_flatten[name] = True
-        #if layer[i].type == 'Deconvolution' or layer[i].type == 39:
-        #    type_string = 'mx.symbol.Deconvolution'
-        #    param_string = convParamToString(layer[i].convolution_param)
-        #    need_flatten[name] = True
+            w_specs, b_specs = wbParamToString(layer[i])
+            param_string += ', ' + w_specs + ', ' + b_specs
         if layer[i].type == 'Pooling' or layer[i].type == 17:
-            type_string = 'singa.layer.Pooling2D'
+            type_string = 'layer.Pooling2D'
             param = layer[i].pooling_param
-            param_string = ''
-            if param.global_pooling == True:
-                # there must be a param `kernel` in a pooling layer
-                param_string += "global_pool=True, kernel=(1,1)"
-            else:
-                param_string += "pad=(%d,%d), kernel=(%d,%d), stride=(%d,%d)" %\
-                    (param.pad, param.pad, param.kernel_size,\
-                    param.kernel_size, param.stride, param.stride)
+            param_string += "pad=(%d,%d), kernel=(%d,%d), stride=(%d,%d)" %\
+                (param.pad, param.pad, param.kernel_size,\
+                param.kernel_size, param.stride, param.stride)
             if param.pool == 0:
                 param_string = param_string + ", mode=model_pb2.PoolingConf.MAX"
             elif param.pool == 1:
                 param_string = param_string + ", mode=model_pb2.PoolingConf.AVE"
             else:
                 raise Exception("Unknown Pooling Method!")
-            need_flatten[name] = True
         if layer[i].type == 'ReLU' or layer[i].type == 18:
-            type_string = 'singa.layer.Activation'
+            type_string = 'layer.Activation'
             param_string = "mode='relu'"
-            need_flatten[name] = need_flatten[mapping[layer[i].bottom[0]]]
         if layer[i].type == 'LRN' or layer[i].type == 15:
-            type_string = 'singa.layer.LRN'
+            type_string = 'layer.LRN'
             param = layer[i].lrn_param
             param_string = "size=%d, alpha=%f, beta=%f, k=%f" %\
                 (param.local_size, param.alpha, param.beta, param.k)
-            need_flatten[name] = True
         if layer[i].type == 'InnerProduct' or layer[i].type == 14:
-            type_string = 'singa.layer.Dense'
+            type_string = 'layer.Dense'
             param = layer[i].inner_product_param
             param_string = "num_output=%d, use_bias=%s" % (param.num_output, param.bias_term)
-            need_flatten[name] = False
+            w_specs, b_specs = wbParamToString(layer[i])
+            param_string += ', ' + w_specs + ', ' + b_specs
         if layer[i].type == 'Dropout' or layer[i].type == 6:
-            type_string = 'singa.layer.Dropout'
+            type_string = 'layer.Dropout'
             param = layer[i].dropout_param
             param_string = "p=%f" % param.dropout_ratio
-            need_flatten[name] = need_flatten[mapping[layer[i].bottom[0]]]
         if layer[i].type == 'Softmax' or layer[i].type == 20:
-            type_string = 'singa.layer.Softmax'
+            type_string = 'layer.Softmax'
+            param = layer[i].softmax_param
+            param_string = "axis=%d" % param.axis
         if layer[i].type == 'Flatten' or layer[i].type == 8:
-            type_string = 'singa.layer.Flatten'
-            need_flatten[name] = False
-        # which model will use Split layer?
-        if layer[i].type == 'Split' or layer[i].type == 22:
-            type_string = 'singa.layer.Split'
-        # no concat layer in singa?
-        if layer[i].type == 'Concat' or layer[i].type == 3:
-            type_string = 'mx.symbol.Concat'
-            need_flatten[name] = True
-        # ??Do we have Crop layer?
-        if layer[i].type == 'Crop':
-            type_string = 'mx.symbol.Crop'
-            need_flatten[name] = True
-            param_string = 'center_crop=True'
-        if layer[i].type == 'BatchNorm':
-            type_string = 'singa.layer.BatchNormalization'
-            param = layer[i].batch_norm_param
+            type_string = 'layer.Flatten'
+            param_string = ''
+        if layer[i].type == 'Accuracy' or layer[i].type == 1:
+            type_string = 'metric.Accuracy'
+            metric = 1
+        if layer[i].type == "SoftmaxWithLoss" or layer[i].type == 21:
+            type_string = 'loss.SoftmaxCrossEntropy'
+            loss = 1
+        if layer[i].type == 'EuclideanLoss' or layer[i].type == 7:
+            type_string = 'loss.SquaredError'
+            loss = 2
+
+        # Caffe don't support BN officially
+        #if layer[i].type == 'BatchNorm':
+        #    type_string = 'singa.layer.BatchNormalization'
+        #    param = layer[i].batch_norm_param
+
         if type_string == '':
             raise Exception('Unknown Layer %s!' % layer[i].type)
-        if type_string != 'split':
-            bottom = layer[i].bottom
-            if param_string != "":
-                param_string = ", " + param_string
-            if len(bottom) == 1:
-                if need_flatten[mapping[bottom[0]]] and type_string == 'mx.symbol.FullyConnected':
-                    flatten_name = "flatten_%d" % flatten_count
-                    symbol_string += "%s=mx.symbol.Flatten(name='%s', data=%s)\n" %\
-                        (flatten_name, flatten_name, mapping[bottom[0]])
-                    flatten_count += 1
-                    need_flatten[flatten_name] = False
-                    bottom[0] = flatten_name
-                    mapping[bottom[0]] = bottom[0]
-                symbol_string += "%s = %s(name='%s', data=%s %s)\n" %\
-                    (name, type_string, name, mapping[bottom[0]], param_string)
-            else:
-                symbol_string += "%s = %s(name='%s', *[%s] %s)\n" %\
-                    (name, type_string, name, ','.join([mapping[x] for x in bottom]), param_string)
-        for j in range(len(layer[i].top)):
-            mapping[layer[i].top[j]] = name
-        output_name = name
-    return symbol_string, output_name, input_dim
 
-def proto2symbol(proto_file):
-    sym, output_name, input_dim = proto2script(proto_file)
-    sym = "import mxnet as mx\n" \
-            + "data = mx.symbol.Variable(name='data')\n" \
-            + sym
-    exec(sym)
-    exec("ret = " + output_name)
-    return ret, input_dim
+        #TODO: conversion of Split, Concat and Crop layers of Caffe
+
+        if layer[i].type == 'InnerProduct':
+            flatten_name = "flatten_%d" % flatten_count
+            output += "\tnet.add(layer.Flatten(name='%s'))\n" % flatten_name
+            flatten_count += 1
+        if layer[i].type != 'Accuracy' and layer[i].type != 'SoftmaxWithLoss' and layer[i].type != 'EuclideanLoss':
+            output += "\tnet.add(%s(%s))\n" % (type_string, '\'' + name + '\', ' + param_string)
+
+        if metric == 1:
+            if loss == 1:
+                output = '\tnet = ffnet.FeedForwardNet(loss.SoftmaxCrossEntropy(), metric.Accuracy())\n' + output
+            elif loss == 2:
+                output = '\tnet = ffnet.FeedForwardNet(loss.SquaredError(), metric.Accuracy())\n' + output
+
+    return output
+
+def solver2script(solver_file):
+    solver = readProtoSolverFile(solver_file)
+    output = ''
+    if solver.solver_mode == 1:
+        output = "\tlayer.engine = 'singacuda'\n"
+    else:
+        output = "\tlayer.engine = 'singacuda'\n"
+
+    return output
+
+def caffe2singa(net_file, solver_file, out_name):
+    script = open(out_name + '.py', 'w')
+    net = net2script(net_file)
+    solver = solver2script(solver_file)
+    script.write('''# sys.path.append(os.path.join(os.path.dirname(__file__), '../../build/python'))
+from singa import layer
+from singa import metric
+from singa import loss
+from singa import net as ffnet
+
+def create_net():
+''')
+    script.write(solver)
+    script.write(net)
+    script.close()
 
 def main():
-    print sys.argv[0]
-    print sys.argv[1]
-    symbol_string, output_name = proto2script(sys.argv[1])
-    if len(sys.argv) > 2:
-        with open(sys.argv[2], 'w') as fout:
-            fout.write(symbol_string)
+    parser = argparse.ArgumentParser(description='Caffe prototxt to SINGA model parameter converter.\
+                    Note that only basic functions are implemented. You are welcomed to contribute to this file.')
+    parser.add_argument('net_prototxt', help='The prototxt file for net in Caffe format')
+    parser.add_argument('solver_prototxt', help='The prototxt file for solver in Caffe format')
+    parser.add_argument('save_model_name', help='The name of the output model prefix')
+    args = parser.parse_args()
+
+    if len(sys.argv) == 4:
+        caffe2singa(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print(symbol_string)
+        print 'Few arguments!'
 
 if __name__ == '__main__':
     main()
